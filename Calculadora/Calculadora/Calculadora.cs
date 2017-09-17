521f1a01986a330756c2f79f7a52144b6f4f5cf3
marinaAC <maru.cardozo02@gmail.com>
2017-09-17 09:29:14 -0300

    subida de tp tarde, anteriormente tuve un problema en el cual, no verifique que no se subio

---
+++ Calculadora/Calculadora/Calculadora.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Calculadora
+    {
+        /// <summary>
+        /// Este metodo recibe dos objetos de tipo numero y un string operador. Llama al metodo validarOperador
+        /// para poder corroborar que es un operador valido. Una vez que este es valido, chequea que 
+        /// cuando sea una division no este diviendo por cero.
+        /// Se hace un get de cada numero, para poder obtener el valor.
+        /// Retorna el resultado de la operacion correspondiente que haga
+        /// </summary>
+        /// <param name="numero1">es el primer numero ingresado por el formulario</param>
+        /// <param name="numero2">es el segundo numero ingresado por formulario</param>
+        /// <param name="operador">operador con el que se desea laburar</param>
+        /// <returns>Retorna el resultado de la operacion</returns>
+        public double Operar(Numero numero1, Numero numero2, string operador) 
+        {
+
+            double returnAux=0;
+
+            switch(ValidarOperador(operador))
+            {
+                case "+":
+                    returnAux = numero1.GetNumero() + numero2.GetNumero();
+                    break;
+                case "-":
+                    returnAux = numero1.GetNumero() - numero2.GetNumero();
+                    break;
+                case "*":
+                    returnAux = numero1.GetNumero() * numero2.GetNumero();
+                    break;
+                case "/":
+                    if (numero2.GetNumero() > 0)
+                    {
+                        returnAux = numero1.GetNumero() / numero2.GetNumero();
+                    }
+                    else 
+                    {
+                        returnAux = 0;
+                    }
+                    break;
+            }
+            
+            return returnAux;
+        }
+
+        /// <summary>
+        ///Recibe un string que corrobora que si es un string invalido devolvera +, sino no sucedera nada. 
+        /// </summary>
+        /// <param name="operador">el operador en formato string</param>
+        /// <returns>retorna un operador valido</returns>
+        private string ValidarOperador(string operador) 
+        {
+            
+            if(operador != "+" && operador != "-"  && operador != "/" && operador != "*")
+            {
+                operador = "+";
+            }
+
+            return operador;
+        }
+
+        
+
+    }
+}
---
+++ Calculadora/Calculadora/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class formCaluladora : Form
+    {
+        static string numero1;
+        static string numero2;
+        static string operatorString;
+        static double resultAux;
+        
+        public formCaluladora()
+        {
+            InitializeComponent();   
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.txtNumero1.Text = null;
+            this.txtNumero2.Text = null;
+            this.cmbOperacion.Text = null;
+            this.lblResultado.Text = null;
+
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            numero1 = this.txtNumero1.Text;
+            numero2 = this.txtNumero2.Text;
+            operatorString = this.cmbOperacion.Text;
+            Numero num = new Numero(numero1);
+            Numero num2 = new Numero(numero2);
+            Calculadora calculoOperar = new Calculadora();
+            resultAux = calculoOperar.Operar(num, num2, operatorString);
+            this.lblResultado.Text = resultAux.ToString();
+
+        }
+
+        private void cmbOperacion_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void txtNumero1_TextChanged(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void txtNumero2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
