6380f80de7df39477e20c1500b1e5b3810e69e71
marinaAC <maru.cardozo02@gmail.com>
2017-11-07 22:08:17 -0300

    pequeñas anotaciones

---
+++ TP4/Navegador TP-4 - AlumnoV2/Archivos/Texto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        protected FileStream archivo;
+        public Texto(string archivo)
+        {
+            FileStream fp = new FileStream(archivo, FileMode.Append);
+            if (!ReferenceEquals(null, fp))
+            {
+                throw new FileNotFoundException("No pudo encontrarse el archivo");
+            }
+            else 
+            {
+                this.archivo = fp;
+            }
+
+        }
+
+
+        public bool guardar(string datos)
+        {
+            bool returnAux = false;
+            BinaryFormatter ser = new BinaryFormatter();
+            if(datos != "")
+            {
+                ser.Serialize(this.archivo, datos);
+                returnAux = true;
+            }
+            this.archivo.Close();
+            return returnAux;
+        }
+
+        public bool leer(out List<string> datos)
+        {
+            bool returnAux = false;
+            BinaryFormatter ser = new BinaryFormatter();
+            //datos = ser.Deserialize(this.archivo);
+            this.archivo.Close();
+        }
+    }
+}
---
+++ TP4/Navegador TP-4 - AlumnoV2/Hilo/Descargador.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public class Descargador
+    {
+        private string html;
+        private Uri direccion;
+
+        public Descargador(Uri direccion)
+        {
+            this.html = "";
+        }
+
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += ;
+                cliente.DownloadStringCompleted += ;
+
+                cliente.DownloadStringAsync();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        //Realizar un hilo, que se va encargar de ejecutar el iniciar
+        //el otro van en paralelo, uno para ver la descarga, y el otro para obtener el html
+
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+        }
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+        }
+    }
+}
